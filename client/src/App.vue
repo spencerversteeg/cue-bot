89ecd80dee1d69ff6a8dbb8c08b6cb646306a29d
Spencer Versteeg <versteeg.spencer@gmail.com>
2020-08-26 12:11:31 -0700

    OAuth Goodies

---
+++ client/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Root Router</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style lang="scss"></style>
---
+++ client/src/views/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Home Vue File</h1>
+    <button @click.prevent="authorizeWithDiscord()">Connect to Discord</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+    authorizeWithDiscord() {
+      const discordAuthentication =
+        "https://discord.com/api/oauth2/authorize?client_id=634923222437068831&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2F&response_type=code&scope=guilds%20identify%20bot%20email";
+      window.open(discordAuthentication);
+    },
+  },
+};
+</script>
