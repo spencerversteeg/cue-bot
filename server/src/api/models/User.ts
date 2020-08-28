868d78d5cb2e56cca5887fadd3a97b6ffabb68bd
Spencer Versteeg <versteeg.spencer@gmail.com>
2020-08-28 08:53:24 -0700

    Added Guild schema

--- server/src/api/models/User.ts
+++ server/src/api/models/User.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document, mongo } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface UserInterface extends Document {
   _id: String;
